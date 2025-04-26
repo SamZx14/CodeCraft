966a6954bef73fb66d40506e8e09fff7a9714415
Samira Ibrahim <samira41104@gmail.com>
2025-04-26 19:16:51 +0600

    exit page

--- a1_choose_menu.c
+++ a1_choose_menu.c
@@ -32,6 +32,7 @@ void dis_menu(int choose,int stp) {
     else printf("%sMain Menu%s\n\n",c2(14),c2(0));
     
     
+    
     printf("%s%sCode Quiz%s\n",c2(11),choose==1?"-> ":"   ",c2(0));
     printf("%s%sSyntax Sprint%s\n",c2(12),choose==2?"-> ":"   ",c2(0));
     printf("%s%sBug Hunter%s\n",c2(13),choose==3?"-> ":"   ",c2(0));
@@ -45,8 +46,43 @@ void dis_menu(int choose,int stp) {
 void dis_exit(){
     system("cls");
 // write the exit code here
+//start code -SAMIRA
+animation("\033[1;33mTHANK YOU FOR PLAYING CODECRAFT !",2);
+printf("%s  (^-^)\n\n\n",c2(10));
 
- 
+animation("\033[0;34m=>> Did You Enjoy The Coding Adventure ?",3);
+printf("%s  (*o*)\n\n",c2(10));
+printf("%sYou've just experienced the amazing world of %sCODECRAFT %sby being our amazing %sCODECRAFTER %swhile having a blast of enjoyment ! \nWe hope you leveled up your programming prowess by aced %sCODE QUIZ %s, raced against logic in %sSYNTAX SPRINT %s, hunted down bugs wildly in %sBUG HUNTER  %sor typed like a pro in %sTYPE MASTER %s.\n\n\n",c2(8),c2(13),c2(8),c2(5),c2(8),c2(2),c2(8),c2(2),c2(8),c2(10),c2(8),c2(2),c2(8));
+
+animation("\033[1;32m=>> WHAT'S NEXT ?",3);
+printf("%s  (*-*)\n\n",c2(10));
+
+animation("\033[0;36mChallenge Yourself Again ",5);
+printf("%s---> Try %sMIX MODE %sfor the ultimate test ...!\n",c2(8),c2(6),c2(8));
+
+animation("\033[0;36mBeat Your High Score ",5);
+printf("%s---> Can you conquer %sHARD %smode ...?\n",c2(8),c2(6),c2(8));
+
+animation("\033[0;36mShare with Friends ",5);
+printf("%s---> Who's the %sBEST CODER %sin your Squad ...?\n\n",c2(8),c2(6),c2(8));
+
+animation("\033[1;35m||> DARE IF YOU CAN DO BETTER NEXT TIME ... DARE IF YOU WANT TO PROVE YOURSELF THE BEST CODECRAFTER EVER  ...<||",4);
+printf("%s  (>-*)\n\n",c2(10));
+
+animation("\033[1;36m=>>Farewell, Future Code Master !",2);
+printf("%s  (>-<)\n\n",c2(10));
+
+animation("\033[1;37mThe best debugger is a good night's sleep ... or another round of ",2);
+printf("%sCODECRAFT !\n\n",c2(13));
+
+animation("\033[1;33m=>>Made With :\n\n",3.5);
+
+animation("\033[1;31m*THE C GURU*",3);
+printf("%s  (*+*)\n\n",c2(10));
+
+animation("\033[1;34m# Keep Coding... Keep Crafting...#\n\n",1.5);
+//end code -SAMIRA
+
 c1(5);
 printf("\nPress any key to return to the menu...\n");
 _getch();
