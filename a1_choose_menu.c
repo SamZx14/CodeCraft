71e682c64ba9f7f4c49db066679ed696c0e2fa19
Samira Ibrahim <samira41104@gmail.com>
2025-04-26 22:04:06 +0600

    last part of about section & more readable exit part

--- a1_choose_menu.c
+++ a1_choose_menu.c
@@ -52,44 +52,18 @@ printf("%s  (^-^)\n\n\n",c2(10));
 
 animation("\033[0;34m=>> Did You Enjoy The Coding Adventure ?",3);
 printf("%s  (*o*)\n\n",c2(10));
-animation("\033[1;37mYou've just experienced the amazing world of ",3);
-
-animation("\033[1;34mCODECRAFT ",3);
-
-animation("\033[1;37mby being our amazing ",3);
-
-animation("\033[0;34mCODECRAFTER ",3);
-
-animation("\033[1;37mwhile having a blast of enjoyment ! \n",3);
-
-animation("\033[1;37mWe hope you leveled up your programming prowess by aced ",3);
-
-animation("\033[1;31mCODE QUIZ ",3);
-
-animation("\033[1;37m, raced against logic in ",3);
-
-animation("\033[1;31mSYNTAX SPRINT ",3);
-
-animation("\033[1;37m, hunted down bugs wildly in ",3);
-
-animation("\033[1;31mBUG HUNTER  ",3);
-
-animation("\033[1;37mor typed like a pro in ",3);
-
-animation("\033[1;31mTYPE MASTER ",3);
-
-animation("\033[1;37m.\n\n\n",3);
+animation("\033[1;37mYou've just experienced the amazing world of \033[1;34mCODECRAFT \033[1;37mby being our amazing \033[0;34mCODECRAFTER \033[1;37mwhile having a blast of enjoyment ! \n\033[1;37mWe hope you leveled up your programming prowess by aced \033[1;31mCODE QUIZ \033[1;37m, raced against logic in \033[1;31mSYNTAX SPRINT \033[1;37m, hunted down bugs\nwildly in \033[1;31mBUG HUNTER  \033[1;37mor typed like a pro in \033[1;31mTYPE MASTER \033[1;37m.\n\n\n",3);
 
 animation("\033[1;32m=>> WHAT'S NEXT ?",3);
 printf("%s  (*-*)\n\n",c2(10));
 
-animation("\033[0;36mChallenge Yourself Again ",5);
+animation("\033[1;32m# \033[0;36mChallenge Yourself Again ",5);
 printf("%s---> Try %sMIX MODE %sfor the ultimate test ...!\n",c2(8),c2(6),c2(8));
 
-animation("\033[0;36mBeat Your High Score ",5);
+animation("\033[1;32m# \033[0;36mBeat Your High Score ",5);
 printf("%s---> Can you conquer %sHARD %smode ...?\n",c2(8),c2(6),c2(8));
 
-animation("\033[0;36mShare with Friends ",5);
+animation("\033[1;32m# \033[0;36mShare with Friends ",5);
 printf("%s---> Who's the %sBEST CODER %sin your Squad ...?\n\n",c2(8),c2(6),c2(8));
 
 animation("\033[1;35m||> DARE IF YOU CAN DO BETTER NEXT TIME ... DARE IF YOU WANT TO PROVE YOURSELF THE BEST CODECRAFTER EVER  ...<||",4);
@@ -120,11 +94,32 @@ system ("cls");
 // write the about code here
 //start code -SAMIRA
 animation("\033[1;34mABOUT CODECRAFT :\n\n\n",2);
+//end code -SAMIRA
 
 
 
+//start code -SAMIRA
+animation("\033[1;36mGame Modes & Difficulty Levels For \033[1;34mCODECRAFTERS \033[1;36m: ",3.5);
 
-c1(3);
+printf("%s  (x.x)\n\n\n",c2(10));
+
+animation("\033[1;32mEasy \033[0;37m, \033[1;33mMedium \033[0;37m, \033[1;31mHard ",4);
+
+animation("\033[0;37m--> Each mini-game features three difficulty levels, ensuring a smooth learning curve.\n\n",4.5);
+
+animation("\033[1;35mMIX MODE ",4);
+
+animation("\033[0;37m--> Feeling adventurous ? \033[1;31m(*.0) \033[0;37mPlay a randomized mix of all four games for the ultimate coding challenge !\n\n\n",4.5);
+
+animation("\033[1;35mWhy Play CODECRAFT ?\n\n",3);
+
+animation("\033[1;32mo \033[0;37mLearn C Programming in a Fun Way\n\033[1;32mo \033[0;37mImprove Debugging & Syntax Skills\n\033[1;32mo \033[0;37mEnhance Typing Speed for Coding\n\033[1;32mo \033[0;37mTest Knowledge with Real-World Challenges\n\033[1;32mo \033[0;37mCompete Against Yourself & Beat High Scores\n\n\033[0;36mDive into \033[1;34mCODECRAFT \033[0;36mand transform coding practice into an exciting adventure ! \n\033[1;37mCan you conquer all levels and become the ultimate \033[0;34mCODECRAFTER ? \033[1;31m(0-0)\n\n\n",5);
+
+animation("\033[1;33mTHE C GURU :\n\n",2);
+
+animation("\033[1;32m* \033[0;34mSayem Islam Leon\n\033[1;32m> \033[1;36mID : \033[0;37m20244103117\n\033[1;32m* \033[0;31mSamira  Ibrahim\n\033[1;32m> \033[1;36mID : \033[0;37m20244103085\n\033[1;32m* \033[0;32mMotmayeen Billah\n\033[1;32m> \033[1;36mID : \033[0;37m20244103083\n\033[1;32m* \033[0;35mSuborna Akter\n\033[1;32m> \033[1;36mID : \033[0;37m20244103092\n\033[1;32m* \033[0;33mFarjana Shanjida Shinzu\n\033[1;32m> \033[1;36mID : \033[0;37m20244103115\n\n",3);
+//end code -SAMIRA
+
 printf("\nPress any key to return to the menu...\n");
 _getch();
 c1(7);
